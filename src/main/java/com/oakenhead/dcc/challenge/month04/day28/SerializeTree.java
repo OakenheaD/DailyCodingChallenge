fccae47c1b45b04df21a3c5e5c97a2b32c678a4e
Yury Sapezhinsky <yury.sapezhinsky@paytah.com>
2019-04-30 16:24:28 +0200

    #000 - work in progress

--- src/main/java/com/oakenhead/dcc/challenge/month04/day28/SerializeTree.java
+++ src/main/java/com/oakenhead/dcc/challenge/month04/day28/SerializeTree.java
@@ -4,9 +4,10 @@ import com.oakenhead.dcc.challenge.AbstractCodingChallenge;
 import com.oakenhead.dcc.challenge.PairValue;
 import com.oakenhead.dcc.challenge.beans.BinaryNode;
 
+import java.util.Arrays;
 import java.util.List;
 
-public class SerializeTree extends AbstractCodingChallenge<Boolean, BinaryNode> {
+public class SerializeTree extends AbstractCodingChallenge<Boolean, PairValue<BinaryNode, String>> {
 
     @Override
     public String dateString() {
@@ -39,12 +40,66 @@ public class SerializeTree extends AbstractCodingChallenge<Boolean, BinaryNode> 
     }
 
     @Override
-    public Boolean runChallengeCase(final BinaryNode input) {
-        return null;
+    public Boolean runChallengeCase(final PairValue<BinaryNode, String> input) {
+
+        return deserializeNode(serializeNode(input.left)).left.left.value.equalsIgnoreCase(input.right);
+
     }
 
     @Override
-    public List<PairValue<Boolean, BinaryNode>> getTestCases() {
-        return null;
+    public List<PairValue<Boolean, PairValue<BinaryNode, String>>> getTestCases() {
+
+        return Arrays.asList(
+                new PairValue<>(true,
+                        new PairValue<>(new BinaryNode("root", new BinaryNode("left", new BinaryNode("left.left")), new BinaryNode("right")), "left.left"))
+        );
+
+    }
+
+    //trivial case - use new Gson();
+    //non trivial case:
+
+    private String serializeNode(final BinaryNode node) {
+
+
+
+    }
+
+    private BinaryNode deserializeNode(final String nodeStr) {
+
+        return
+
+    }
+
+    private String serializeNodeRecursive(final BinaryNode node) {
+
+        final String leftValue = node.left == null ? "null" : serializeNodeRecursive(node.left);
+        final String rightValue = node.right == null ? "null" : serializeNodeRecursive(node.right);
+        final String mainValueSanitized = !node.value.contains("#") ?
+                node.value :
+                node.value.replaceAll("&", "&ampersand_san;").replaceAll("#", "&hash_sanitized;");
+
+        return String.format("{%s#%s#%s}", mainValueSanitized, leftValue, rightValue);
+
+    }
+
+    private BinaryNode deserializeNodeRecursive(final String nodeStr) {
+
+        if ("null".equalsIgnoreCase(nodeStr)) {
+            return null;
+        }
+
+        final String withoutBraces = nodeStr.substring(1, nodeStr.length() - 1);
+        final String[] parts = nodeStr.split("#");
+
+        final String mainValueUnSanitized = !parts[0].contains("&hash_sanitized;") ?
+                parts[0] :
+                parts[0].replaceAll("&hash_sanitized;", "#").replaceAll("&ampersand_san;","&");
+
+        final BinaryNode leftValue = node.left == null ? "null" : serializeNodeRecursive(node.left);
+        final BinaryNode rightValue = node.right == null ? "null" : serializeNodeRecursive(node.right);
+
+        return new BinaryNode()
+
     }
 }
