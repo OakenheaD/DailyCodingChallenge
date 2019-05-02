b9ae5d1f978797df0f39a14c6d4d1435710c2744
Yury Sapezhinsky <yury.sapezhinsky@paytah.com>
2019-05-02 12:22:44 +0200

    #003 - timing optimisation

--- src/main/java/com/oakenhead/dcc/challenge/month04/day27/ArrayMultiplication.java
+++ src/main/java/com/oakenhead/dcc/challenge/month04/day27/ArrayMultiplication.java
@@ -5,6 +5,7 @@ import com.oakenhead.dcc.challenge.CodingChallenge;
 import com.oakenhead.dcc.challenge.TripleValue;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
@@ -43,11 +44,32 @@ public class ArrayMultiplication extends AbstractCodingChallenge<List<Long>, Lis
         return input.stream().map(num -> product / num).collect(Collectors.toList());
 
     }
+    public List<Long> runChallengeCaseNonStream(final List<Long> input) {
+
+        long product = 1;
+
+        for (int i = 0; i < input.size(); i++) {
+
+            product *= input.get(i);
+
+        }
+
+        final List<Long> result = new ArrayList<>(input.size());
+
+        for (int i = 0; i < input.size(); i++) {
+
+            result.add(i, product / input.get(i));
+
+        }
+
+        return result;
+    }
 
     @Override
     public List<TripleValue<List<Long>, Function<List<Long>, List<Long>>, List<Long>>> getTestCases() {
 
-        final Function<List<Long>, List<Long>> testFunction = this::runChallengeCase;
+        //final Function<List<Long>, List<Long>> testFunction = this::runChallengeCase;
+        final Function<List<Long>, List<Long>> testFunction = this::runChallengeCaseNonStream;
 
         return Arrays.asList(
                 new TripleValue<>(Arrays.asList(120L, 60L, 40L, 30L, 24L), testFunction, Arrays.asList(1L, 2L, 3L, 4L, 5L)),
