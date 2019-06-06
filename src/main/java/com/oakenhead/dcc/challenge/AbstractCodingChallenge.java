2e899bdf1b2c23ce397067a1c0dbc17fbbe12dd0
Yury Sapezhinsky <yury.sapezhinsky@paytah.com>
2019-06-06 16:02:17 +0200

    Array results comparison case.

--- src/main/java/com/oakenhead/dcc/challenge/AbstractCodingChallenge.java
+++ src/main/java/com/oakenhead/dcc/challenge/AbstractCodingChallenge.java
@@ -3,7 +3,10 @@ package com.oakenhead.dcc.challenge;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
 
 public abstract class AbstractCodingChallenge<R, T> implements CodingChallenge<R, T> {
@@ -20,12 +23,24 @@ public abstract class AbstractCodingChallenge<R, T> implements CodingChallenge<R
         final long challengeBegin = System.nanoTime();
 
         final Boolean result =  testCases.stream()
-                .mapToInt(testCase -> testCase.middle.apply(testCase.right).equals(testCase.left) ? 1 : 0)
+                .mapToInt(testCase -> compateSupposedAndActualCase(testCase.middle.apply(testCase.right),testCase.left))
                 .sum() == testCases.size();
 
         final long challengeEnd = System.nanoTime();
 
         return new QuadValue<>(challengeBegin, challengeEnd, testCases.size(), result);
+
+    }
+
+    private int compateSupposedAndActualCase(final R testCase, final R supposedCase) {
+
+        final boolean isArray = supposedCase.getClass().isArray();
+
+        if (isArray) {
+            return Arrays.deepEquals((Object[])testCase, (Object[]) supposedCase) ? 1 : 0;
+        }
+
+        return Objects.equals(testCase, supposedCase)? 1 : 0;
 
     }
 
---
+++ src/main/java/com/oakenhead/dcc/challenge/month05/day13/MaxValueInSubArrayChallenge.java
@@ -0,0 +1,76 @@
+package com.oakenhead.dcc.challenge.month05.day13;
+
+import com.oakenhead.dcc.challenge.AbstractCodingChallenge;
+import com.oakenhead.dcc.challenge.PairValue;
+import com.oakenhead.dcc.challenge.TripleValue;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+@Service
+public class MaxValueInSubArrayChallenge extends AbstractCodingChallenge<Integer[] , PairValue<Integer[], Integer>> {
+
+    @Override
+    public String dateString() {
+        return "2019 May 13";
+    }
+
+    @Override
+    public String shortName() {
+        return "maximum values of each subarray";
+    }
+
+    @Override
+    public String getChallengeTask() {
+        return "Good morning! Here's your coding interview problem for today.\n" +
+                "\n" +
+                "This problem was asked by Google.\n" +
+                "\n" +
+                "Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum values of each subarray of length k.\n" +
+                "\n" +
+                "For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we should get: [10, 7, 8, 8], since:\n" +
+                "\n" +
+                "    10 = max(10, 5, 2)\n" +
+                "    7 = max(5, 2, 7)\n" +
+                "    8 = max(2, 7, 8)\n" +
+                "    8 = max(7, 8, 7)\n" +
+                "\n" +
+                "Do this in O(n) time and O(k) space. You can modify the input array in-place and you do not need to store the results. You can simply print them out as you compute them.";
+    }
+
+    @Override
+    public Integer[] runChallengeCase(final PairValue<Integer[], Integer> input) {
+
+        final int subarrayLength = input.right;
+        final Integer[] data = input.left;
+        final Integer[] result = new Integer[data.length - subarrayLength + 1];
+
+        //o(nk)
+        for (int i = 0; i < result.length; i++) {
+
+            int currentMax = data[i];
+
+            for (int j = 1; j < subarrayLength; j++) {
+                currentMax = Math.max(currentMax, data[i + j]);
+            }
+
+            result[i] = currentMax;
+
+        }
+
+        return result;
+    }
+
+    @Override
+    public List<TripleValue<Integer[], Function<PairValue<Integer[], Integer>, Integer[]>, PairValue<Integer[], Integer>>> getTestCases() {
+        final Function<PairValue<Integer[], Integer>, Integer[]> testFunction = this::runChallengeCase;
+        final Integer[] testInput = new Integer[] {10, 5, 2, 7, 8, 7};
+        final Integer[] testResult = new Integer[] {10, 7, 8, 8};
+
+        return Arrays.asList(
+                new TripleValue<>(testResult, testFunction, new PairValue<>(testInput, 3))
+        );
+    }
+}
