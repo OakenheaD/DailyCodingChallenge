a228bf61277c0d7995850c1b67d62bd447355f86
Yury Sapezhinsky <yury.sapezhinsky@paytah.com>
2019-06-28 10:26:40 +0200

    PAY-202 - Intersecting Lists Challenge.

--- src/main/java/com/oakenhead/dcc/challenge/month05/day15/IntersectingListsChallenge.java
+++ src/main/java/com/oakenhead/dcc/challenge/month05/day15/IntersectingListsChallenge.java
@@ -3,12 +3,16 @@ package com.oakenhead.dcc.challenge.month05.day15;
 import com.oakenhead.dcc.challenge.AbstractCodingChallenge;
 import com.oakenhead.dcc.challenge.PairValue;
 import com.oakenhead.dcc.challenge.TripleValue;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.Function;
 
+@Service
 public class IntersectingListsChallenge extends AbstractCodingChallenge<Integer, PairValue<LinkedList<Integer>, LinkedList<Integer>>> {
 
 
@@ -37,14 +41,28 @@ public class IntersectingListsChallenge extends AbstractCodingChallenge<Integer,
 
     @Override
     public Integer runChallengeCase(final PairValue<LinkedList<Integer>, LinkedList<Integer>> input) {
-        return null;
+
+        final HashMap<Integer, Integer> listOneMap = input.left.stream().collect(HashMap::new, (map, entry) -> map.put(entry, entry), HashMap::putAll);
+
+        Iterator<Integer> listTwoInteger = input.right.iterator();
+
+        while(listTwoInteger.hasNext()) {
+
+            final Integer next = listTwoInteger.next();
+            if (listOneMap.containsKey(next)) {
+                return next;
+            }
+
+        }
+
+        return Integer.MIN_VALUE;
     }
 
     @Override
     public List<TripleValue<Integer, Function<PairValue<LinkedList<Integer>, LinkedList<Integer>>, Integer>, PairValue<LinkedList<Integer>, LinkedList<Integer>>>> getTestCases() {
 
         final Function<PairValue<LinkedList<Integer>, LinkedList<Integer>>, Integer> testFunction = this::runChallengeCase;
-        
+
         final LinkedList<Integer> listOne = new LinkedList<>(Arrays.asList(3, 7, 8, 10));
         final LinkedList<Integer> listTwo = new LinkedList<>(Arrays.asList(99, 1, 8, 10));
 
